c490f420f889f3fd50370f40f8f773b8c70c1b99
David Nguyen <davidnguyen21@gmail.com>
2018-04-19 12:43:56 -0700

    Remove dupe code

--- tb-kanban/src/components/KanBanColumnHeader.js
+++ tb-kanban/src/components/KanBanColumnHeader.js
@@ -11,17 +11,6 @@ const Header = styled.div`
   line-height: 30px;
 `;
 
-const header = columnIndex => ({
-  height: 30,
-  backgroundColor: colors[columnIndex],
-  width: "100%",
-  textAlign: "center",
-  color: "white",
-  lineHeight: 2
-});
-
-const KanBanColumnHeader = ({ name, columnIndex }) => {
-  return <div style={header(columnIndex)}>{name}</div>;
 const KanBanColumnHeader = ({
   columnName,
   columnColor,
